58a6d4958fb69a29af579187e34b1727c4af8f44
Ryan Fan <reg_info@qq.com>
2019-02-19 19:39:24 +0800

    Create upgrade_to_utf8mb4.py

---
+++ upgrade_to_utf8mb4.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+import MySQLdb
+
+host = "localhost"
+passwd = ""
+user = "root"
+dbname = "mall"
+
+db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=dbname)
+cursor = db.cursor()
+
+cursor.execute("ALTER DATABASE `%s` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'" % dbname)
+
+sql = "SELECT DISTINCT(table_name) FROM information_schema.columns WHERE table_schema = '%s'" % dbname
+cursor.execute(sql)
+
+results = cursor.fetchall()
+for row in results:
+  try:
+    sql = "ALTER TABLE `%s` convert to character set DEFAULT COLLATE DEFAULT" % (row[0])
+    cursor.execute(sql)
+  except:
+    cursor.execute("DESC `%s`" % (row[0]))
+    desc = cursor.fetchall()
+    for elem in desc:
+      if "varchar" in elem[1]: #data type of the columns
+        try:
+            cursor.execute("ALTER TABLE `%s` MODIFY `%s` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci" % (row[0], elem[0]))
+        except:
+            print("ALTER TABLE `%s` MODIFY `%s` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci" % (row[0], elem[0]))
+            continue
+db.close()
